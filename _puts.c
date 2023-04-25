e389383846e8327df32b56f9df36df50f93ca0b9
Mouna <mounaouriagli2@gmail.com>
2023-04-24 18:18:07 -0700

    printf_team_project

---
+++ _puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _puts - print string to stdin
+ * @str: string
+ * Return: int number of chars printed
+ */
+int _puts(char *str)
+{
+	int printed = _strlen(str);
+
+	write(1, str, printed);
+	return (printed);
+}
